0f6e3748e5c21a22ba54ab9d51f6f6d56e97ab17
brian <brian@barnescocke.com>
2020-07-13 10:49:36 -0700

    fix walk dirs

--- src/shardBackup/walkDirs.py
+++ src/shardBackup/walkDirs.py
@@ -1,4 +1,18 @@
-from pathlib import pathlib
+import os
 
-for path in Path('.'):
-    print(path)
+# Print every file with its size recursing through dirs // https://www.devdungeon.com/content/walk-directory-python
+def recurse_dir(root_dir):
+    """
+    Used to walk a given dir. recursively and print out files
+    >>>
+    >>>root_dir("")
+    shardBackup/Pipfile - 138 bytes
+    """
+    root_dir = os.path.abspath(root_dir)
+    for item in os.listdir(root_dir):
+        item_full_path = os.path.join(root_dir, item)
+
+        if os.path.isdir(item_full_path):
+            recurse_dir(item_full_path)
+        else:
+            print("%s - %s bytes" % (item_full_path, os.stat(item_full_path).st_size))
