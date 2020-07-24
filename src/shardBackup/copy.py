e5c5480eb373b7c4ad4a27ae13c86993a307d0f2
brian <brian@barnescocke.com>
2020-07-24 13:28:41 -0700

    copy functionality

--- src/shardBackup/copy.py
+++ src/shardBackup/copy.py
@@ -1,18 +1,32 @@
-import subprocess
-import sys
+from subprocess import run
+from sys import exit
+from shutil import copy2
+from os import chown, stat
+import stat
 
-def rsync(fobject0, fobject1):
+def rsync(fobject0, fobject1): # takes two file objects and transmits 0 to 1
     """
     a call to copying using rsync
+    >>>rsync('','')
+
     """
     try:
-        subprocess.run(['rsync', #rsync is a major program
+        run(['rsync', #rsync is a major program
             '-avzz', #a = archive, v= verbose, zz=compress 
             '-n', # n = simulate
-            '--info=progress2' # prints info such as how fast it is downloading
-            'fobject0'
-            'fobject1'
+            '--info=progress2', # prints info such as how fast it is downloading
+            fobject0,
+            fobject1
             ])
     except:
         print(f"Unable to launch rsync copying - despite finding rsync installed")
-        sys.exit(1)
+        exit(1)
+
+def copy(fobject0, fobject1): #copies file and then changes perms/owner
+    try:
+        copy2(fobject0, fobject1)
+        st = os.stat(fobject0)
+        chown(target, st[stat.ST_UID], st[stat.ST_GID])
+    except:
+        print(f"Unable to copy {fobject0} to {fobject1}. I told you it was in alpha.")
+        exit(1)
