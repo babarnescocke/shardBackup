c1e2c2f47571e23b0b4dfa19408bf2ba5337a9dc
brian <brian@barnescocke.com>
2020-07-23 21:40:21 -0700

    fixed setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open('README.md', 'r') as f:
 
 setup(
     name='shardbackup',
-    version='0.0.1',
+    version='0.0.2',
     author='Brian A Barnes-Cocke',
-    author_email='not@yet.com',
+    author_email='bacocke08@gmail.com',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/linuxacademy/pgbackup',
+    url='https://github.com/babarnescocke/shardBackup',
     packages=find_packages('src'),
     package_dir={'': 'src'},
 )
