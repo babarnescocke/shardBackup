42c8496adc0e7baae5d096f4c37ac0c4505a6afa
brian <brian@barnescocke.com>
2020-07-24 20:33:36 -0700

    recurseDIR working

--- src/shardBackup/walkDirs.py
+++ src/shardBackup/walkDirs.py
@@ -8,11 +8,23 @@ def recurse_dir(root_dir):
     >>>root_dir("")
     shardBackup/Pipfile - 138 bytes
     """
-    root_dir = os.path.abspath(root_dir)
+    root_dir = os.path.relpath(root_dir)
     for item in os.listdir(root_dir):
         item_full_path = os.path.join(root_dir, item)
-
         if os.path.isdir(item_full_path):
             recurse_dir(item_full_path)
         else:
-            print("%s - %s bytes" % (item_full_path, os.stat(item_full_path).st_size))
+            print(f"{os.stat(item_fu    ll_path).st_size}   {item_full_path}")
+
+
+""" pseudo code:
+size = 0b
+dsize = 4k
+targsize = lots of bytes
+walk dir
+  if file
+    fsize = file_size + 131 bytes + len("basename of file".encode('utf-8'))
+    if fsize + size <= targetsize
+  if dir
+    dsize + 
+"""
